a24c2f669187187a01889f1fa014a4401aa24a2b
kento-oka <kento-oka@kentoka.com>
2018-08-04 23:15:26 +0900

    レスポンスファクトリもたぶんできた

---
+++ src/ResponseFactory.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * FratilyPHP Http Message
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author      Kento Oka <kento-oka@kentoka.com>
+ * @copyright   (c) Kento Oka
+ * @license     MIT
+ * @since       1.0.0
+ */
+namespace Fratily\Http\Message;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
+
+/**
+ *
+ */
+class ResponseFactory implements ResponseFactoryInterface{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function createResponse(int $code = 200, string $reasonPhrase = ''): ResponseInterface{
+        return new Response($code);
+    }
+}
